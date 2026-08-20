7633b4be2554de3267d24e902b06abe99a384149
Marcus Hermansson <marcus@hmwh.se>
2026-08-20 21:47:10 +0200

    Import subexponential Laplace integrability seam

--- Zeta23/ExceptionalZero/Final.lean
+++ Zeta23/ExceptionalZero/Final.lean
@@ -13,6 +13,7 @@ import Zeta23.ExceptionalZero.FilteredLaplace
 import Zeta23.ExceptionalZero.FilteredZeroRegularity
 import Zeta23.ExceptionalZero.FilteredGrowthEnvelope
 import Zeta23.ExceptionalZero.MeromorphicIdentity
+import Zeta23.ExceptionalZero.FilteredLaplaceSubexp
 import Zeta23.ExceptionalZero.PoleKiller
 import Zeta23.ExceptionalZero.ConditionalClosure
 import Zeta23.ExceptionalZero.WeilBridge
