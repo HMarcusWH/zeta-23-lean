c0a2586b57bf33c3830e2b2034f071a2b397cfd8
Marcus Hermansson <marcus@hmwh.se>
2026-08-22 21:07:27 +0200

    Build dictionary tent zero-summability through CCM

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -15,5 +15,6 @@ import Zeta23.CCM.DictionaryResidualSecondOrderGluing
 import Zeta23.CCM.DictionaryTent
 import Zeta23.CCM.DictionaryTentTransform
 import Zeta23.CCM.DictionaryTentDecay
+import Zeta23.CCM.DictionaryTentZeroSummability
 import Zeta23.CCM.Displacement
 import Zeta23.CCM.ClaimBindings
