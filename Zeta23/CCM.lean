61894a65fe607b2005ac105ca890ee9630726006
Marcus Hermansson <marcus@hmwh.se>
2026-08-23 20:12:58 +0200

    R003: import arch gamma bridge

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -23,4 +23,5 @@ import Zeta23.CCM.DictionaryPoleCompletion
 import Zeta23.CCM.Displacement
 import Zeta23.CCM.DictionaryPoleSource
 import Zeta23.CCM.DictionaryArchPhysical
+import Zeta23.CCM.DictionaryArchBridge
 import Zeta23.CCM.ClaimBindings
