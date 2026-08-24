87d996612d27822725b505f65f92dd8f0f0505c8
HMarcusWH <Marcus@hmwh.se>
2026-08-24 23:57:27 +0200

    CCM: close off-diagonal arch entries

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -35,4 +35,5 @@ import Zeta23.CCM.DictionaryArchMuGrowth
 import Zeta23.CCM.DictionaryArchSourceMuIntegrability
 import Zeta23.CCM.DictionaryArchDensityBridge
 import Zeta23.CCM.DictionaryArchSourceBridge
+import Zeta23.CCM.DictionaryArchOffDiagonal
 import Zeta23.CCM.ClaimBindings
