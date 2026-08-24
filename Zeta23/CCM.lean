66fc62f122a1c3d695fe74fe37cab6eec79ee0fe
Marcus Hermansson <marcus@hmwh.se>
2026-08-24 02:59:29 +0200

    R003: compile arch completion module in CCM surface

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -25,4 +25,5 @@ import Zeta23.CCM.DictionaryPoleSource
 import Zeta23.CCM.DictionaryArchPhysical
 import Zeta23.CCM.DictionaryArchBridge
 import Zeta23.CCM.DictionaryArchLaplace
+import Zeta23.CCM.DictionaryArchCompletion
 import Zeta23.CCM.ClaimBindings
