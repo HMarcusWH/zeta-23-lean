f8d6a8fc3843f4d13e83b3a2c3e51050334a3ec9
Marcus Hermansson <marcus@hmwh.se>
2026-08-28 02:13:18 +0200

    T-1M M2: integrate mollifier transform gate

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -17,6 +17,7 @@ import Zeta23.CCM.DictionaryTentTransform
 import Zeta23.CCM.DictionaryTentDecay
 import Zeta23.CCM.DictionaryTentZeroSummability
 import Zeta23.CCM.DictionaryTentMollifier
+import Zeta23.CCM.DictionaryTentMollifierTransform
 import Zeta23.CCM.DictionaryDeterministicRHS
 import Zeta23.CCM.DictionarySourceTest
 import Zeta23.CCM.DictionaryPole
