5cbec2abc1efc6293f6752ef36e0dffcb845c279
Marcus Hermansson <marcus@hmwh.se>
2026-08-28 03:17:45 +0200

    T-1M: promote M3 support architecture into CCM build

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -18,6 +18,7 @@ import Zeta23.CCM.DictionaryTentDecay
 import Zeta23.CCM.DictionaryTentZeroSummability
 import Zeta23.CCM.DictionaryTentMollifier
 import Zeta23.CCM.DictionaryTentMollifierTransform
+import Zeta23.CCM.DictionaryTentMollifierSupport
 import Zeta23.CCM.DictionaryDeterministicRHS
 import Zeta23.CCM.DictionarySourceTest
 import Zeta23.CCM.DictionaryPole
