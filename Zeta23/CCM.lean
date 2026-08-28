0dc1896b6b353121182e568b059a02cc9990aa9b
Marcus Hermansson <marcus@hmwh.se>
2026-08-28 17:25:36 +0200

    H2b: wire zero-side summability into CCM build

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -52,4 +52,5 @@ import Zeta23.CCM.DictionarySmoothCoreBridge
 import Zeta23.CCM.DictionarySymmetry
 import Zeta23.CCM.DictionarySmoothCorePolarization
 import Zeta23.CCM.CodimOneMatrixCompletion
+import Zeta23.CCM.DictionaryZeroSideSummability
 import Zeta23.CCM.ClaimBindings
