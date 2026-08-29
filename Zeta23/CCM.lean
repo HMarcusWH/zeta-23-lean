8238e53516bdcf47671248824fb9c00b58fe9e92
Marcus Hermansson <marcus@hmwh.se>
2026-08-29 19:32:05 +0200

    R003 M6: export archimedean tent limit

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -21,6 +21,7 @@ import Zeta23.CCM.DictionaryTentMollifierTransform
 import Zeta23.CCM.DictionaryTentMollifierSupport
 import Zeta23.CCM.DictionaryTentMollifierLimit
 import Zeta23.CCM.DictionaryTentZeroLimit
+import Zeta23.CCM.DictionaryTentArchLimit
 import Zeta23.CCM.DictionaryDeterministicRHS
 import Zeta23.CCM.DictionaryTentPolePrimeLimit
 import Zeta23.CCM.DictionarySourceTest
