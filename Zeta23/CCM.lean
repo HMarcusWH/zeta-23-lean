05fccc4e8e275ec97534f976fea831ad634cb2dc
Marcus Hermansson <marcus@hmwh.se>
2026-08-30 18:07:32 +0200

    D0-C: export cutoff-free matrix theorem surface

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -65,4 +65,5 @@ import Zeta23.CCM.DictionaryZeroSideBridge
 import Zeta23.CCM.DictionaryKernelEFExtension
 import Zeta23.CCM.DictionaryZeroSideDisplacement
 import Zeta23.CCM.RawKernelZeroSideBridge
+import Zeta23.CCM.CutoffFreeMatrix
 import Zeta23.CCM.ClaimBindings
