cb7c532c48e437536a4b0e9ab61f4d4247857b79
Marcus Hermansson <marcus@hmwh.se>
2026-09-06 15:38:18 +0200

    Export explicit cubic secular bridge

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -92,5 +92,6 @@ import Zeta23.CCM.FirstBadShiftedSchur
 import Zeta23.CCM.CubicShellIncidence
 import Zeta23.CCM.CubicNormalizedSchur
 import Zeta23.CCM.CubicSecularEquation
+import Zeta23.CCM.CubicExplicitSecular
 import Zeta23.CCM.SourceKappa
 import Zeta23.CCM.ClaimBindings
