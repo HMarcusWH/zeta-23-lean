16e97bd469d18ba7477a675f4ff75d9a3114034a
Marcus Hermansson <marcus@hmwh.se>
2026-09-10 03:15:27 +0200

    CCM: wire frozen intrinsic predecessor scaffold

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -106,6 +106,7 @@ import Zeta23.CCM.CanonicalSourceMomentAtoms
 import Zeta23.CCM.CanonicalApertureRegularityScaffold
 import Zeta23.CCM.CanonicalApertureContinuity
 import Zeta23.CCM.FrozenCanonicalSourceAnalytic
+import Zeta23.CCM.FrozenIntrinsicPredecessorAnalytic
 import Zeta23.CCM.SourceMomentDecomposition
 import Zeta23.CCM.CrossParitySecularTransfer
 import Zeta23.CCM.KernelSourceTransport
