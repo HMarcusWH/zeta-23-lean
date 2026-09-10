36808dff62dcbaeb08addde08bb5b42042003dc4
Marcus Hermansson <marcus@hmwh.se>
2026-09-10 12:49:54 +0200

    CCM: wire complex aperture bridge and source modules

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -106,6 +106,8 @@ import Zeta23.CCM.CanonicalSourceMomentAtoms
 import Zeta23.CCM.CanonicalApertureRegularityScaffold
 import Zeta23.CCM.CanonicalApertureContinuity
 import Zeta23.CCM.CanonicalApertureAnalyticPrimitives
+import Zeta23.CCM.CanonicalApertureComplexBridge
+import Zeta23.CCM.CanonicalApertureComplexSource
 import Zeta23.CCM.FrozenCanonicalSourceAnalytic
 import Zeta23.CCM.FrozenIntrinsicPredecessorAnalytic
 import Zeta23.CCM.SourceMomentDecomposition
