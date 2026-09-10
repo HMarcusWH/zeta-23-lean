3c98dab77b6d1b2c4dca5b6de199af12da2a2f75
Marcus Hermansson <marcus@hmwh.se>
2026-09-10 03:13:24 +0200

    CCM: wire frozen source analytic scaffold

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -105,6 +105,7 @@ import Zeta23.CCM.QuadraticNormalMatrixMoment
 import Zeta23.CCM.CanonicalSourceMomentAtoms
 import Zeta23.CCM.CanonicalApertureRegularityScaffold
 import Zeta23.CCM.CanonicalApertureContinuity
+import Zeta23.CCM.FrozenCanonicalSourceAnalytic
 import Zeta23.CCM.SourceMomentDecomposition
 import Zeta23.CCM.CrossParitySecularTransfer
 import Zeta23.CCM.KernelSourceTransport
