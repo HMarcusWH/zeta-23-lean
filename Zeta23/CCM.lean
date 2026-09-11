cd4c9c05ebf044968dcefa3bcbbbd86dbbd6004e
Marcus Hermansson <marcus@hmwh.se>
2026-09-11 22:49:59 +0200

    CCM: wire cell-minimal regular first-bad selection

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -120,6 +120,7 @@ import Zeta23.CCM.CanonicalApertureSourceHolomorphy
 import Zeta23.CCM.FrozenIntrinsicPredecessorHolomorphy
 import Zeta23.CCM.LiftedPredecessorDeterminantRigidity
 import Zeta23.CCM.LiftedPredecessorRegularity
+import Zeta23.CCM.CellMinimalRegularFirstBad
 import Zeta23.CCM.SourceMomentDecomposition
 import Zeta23.CCM.CrossParitySecularTransfer
 import Zeta23.CCM.KernelSourceTransport
