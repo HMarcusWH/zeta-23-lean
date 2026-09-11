b0f121aef53376aca69fc646ca8b82a0c611c7af
Marcus Hermansson <marcus@hmwh.se>
2026-09-11 22:51:03 +0200

    CCM: wire regular first-bad canonical energy

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -126,6 +126,7 @@ import Zeta23.CCM.CrossParitySecularTransfer
 import Zeta23.CCM.KernelSourceTransport
 import Zeta23.CCM.ZeroShiftCrossParityTransfer
 import Zeta23.CCM.CanonicalSourceEnergy
+import Zeta23.CCM.RegularFirstBadCanonicalEnergy
 import Zeta23.CCM.CanonicalSourcePairing
 import Zeta23.CCM.CanonicalOneStepDomination
 import Zeta23.CCM.SourceKappa
