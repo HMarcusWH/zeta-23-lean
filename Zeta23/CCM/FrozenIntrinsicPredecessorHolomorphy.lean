ad501e1d9c384650be955848aa31d41707f50d2b
Marcus Hermansson <marcus@hmwh.se>
2026-09-12 00:46:32 +0200

    CCM: use inherited normed space for intrinsic predecessor

--- Zeta23/CCM/FrozenIntrinsicPredecessorHolomorphy.lean
+++ Zeta23/CCM/FrozenIntrinsicPredecessorHolomorphy.lean
@@ -102,11 +102,8 @@ theorem analyticOnNhd_complexFrozenIntrinsicPredecessorRemainder_apply_sourceDom
     AnalyticOnNhd ℂ
       (fun z : ℂ => complexFrozenIntrinsicPredecessorRemainder Q p N z x)
       complexFrozenSourceDomain := by
-  letI : NormedSpace ℂ (intrinsicParityPredecessorSubspace p N) where
-    norm_smul_le c y := by
-      simpa using
-        (norm_smul_le c
-          (y : euclideanParityBoundaryFlatSubspace p (N + 1)))
+  letI : NormedSpace ℂ (intrinsicParityPredecessorSubspace p N) :=
+    (intrinsicParityPredecessorSubspace p N).normedSpace
   let P := (intrinsicPredecessorPart p N).toContinuousLinearMap
   have hpar :=
     analyticOnNhd_complexFrozenParityCompressedRemainder_apply_sourceDomain
