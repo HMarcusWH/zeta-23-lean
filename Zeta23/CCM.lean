71f4eccacae9c9642a1a272e45175b995a13d42a
Marcus Hermansson <marcus@hmwh.se>
2026-09-12 22:09:05 +0200

    CCM: wire FB-03 discrepancy smoothing modules

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -127,6 +127,8 @@ import Zeta23.CCM.KernelSourceTransport
 import Zeta23.CCM.ZeroShiftCrossParityTransfer
 import Zeta23.CCM.CanonicalSourceEnergy
 import Zeta23.CCM.CanonicalPolePrimeDiscrepancy
+import Zeta23.CCM.CanonicalPolePrimeDiscrepancyIntegrability
+import Zeta23.CCM.CanonicalPolePrimeRiesz
 import Zeta23.CCM.RegularFirstBadCanonicalEnergy
 import Zeta23.CCM.CanonicalSourcePairing
 import Zeta23.CCM.CanonicalOneStepDomination
