140e88b570b944b4c5705cb109293975e628463e
Marcus Hermansson <marcus@hmwh.se>
2026-09-13 01:58:11 +0200

    build: wire source transport and Riesz closure

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -127,10 +127,13 @@ import Zeta23.CCM.KernelSourceTransport
 import Zeta23.CCM.ZeroShiftCrossParityTransfer
 import Zeta23.CCM.CanonicalSourceEnergy
 import Zeta23.CCM.CanonicalPolePrimeDiscrepancy
+import Zeta23.CCM.SourceDerivativeTransport
 import Zeta23.CCM.CanonicalSourceEnergyJets
 import Zeta23.CCM.CanonicalPolePrimeDiscrepancyIntegrability
 import Zeta23.CCM.CanonicalPolePrimeRiesz
+import Zeta23.CCM.CanonicalSourceRieszEnergy
 import Zeta23.CCM.RegularFirstBadCanonicalEnergy
+import Zeta23.CCM.RegularFirstBadRieszEnergy
 import Zeta23.CCM.CanonicalSourcePairing
 import Zeta23.CCM.CanonicalOneStepDomination
 import Zeta23.CCM.SourceKappa
