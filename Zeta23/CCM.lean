b165292477182d81911a487441262e1778d848d5
Marcus Hermansson <marcus@hmwh.se>
2026-09-23 02:55:09 +0200

    PR #247: wire strict-odd global-bottom normal form

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -167,6 +167,7 @@ import Zeta23.CCM.GlobalFirstBadParityBottom
 import Zeta23.CCM.GlobalFirstBadParityBottomAlignment
 import Zeta23.CCM.GlobalParityBottomRetainedGeometry
 import Zeta23.CCM.GlobalParityBottomEvenStrictNormalForm
+import Zeta23.CCM.GlobalParityBottomOddStrictNormalForm
 import Zeta23.CCM.CrossParitySecularCompletion
 import Zeta23.CCM.CrossParitySecularKernelDichotomy
 import Zeta23.CCM.RegularFirstBadParitySourceMomentRigidity
