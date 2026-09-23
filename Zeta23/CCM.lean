7d902efa2c10bfaef6c0f2e8e9e7d1669871f2fb
Marcus Hermansson <marcus@hmwh.se>
2026-09-23 02:57:46 +0200

    PR #247: wire residual prime-remainder normal form

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -171,6 +171,7 @@ import Zeta23.CCM.GlobalParityBottomOddStrictNormalForm
 import Zeta23.CCM.GlobalParityBottomReverseGroundTransfer
 import Zeta23.CCM.GlobalParityBottomTieNormalForm
 import Zeta23.CCM.GlobalParityBottomResidualState
+import Zeta23.CCM.GlobalParityBottomPrimeRemainder
 import Zeta23.CCM.CrossParitySecularCompletion
 import Zeta23.CCM.CrossParitySecularKernelDichotomy
 import Zeta23.CCM.RegularFirstBadParitySourceMomentRigidity
