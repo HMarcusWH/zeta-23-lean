e714210eee8d237ffc385a24ddf1ccbfe35bece7
Marcus Hermansson <marcus@hmwh.se>
2026-09-23 03:32:10 +0200

    PR #247: wire typed branch and ground-trial layers

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -171,6 +171,8 @@ import Zeta23.CCM.GlobalParityBottomOddStrictNormalForm
 import Zeta23.CCM.GlobalParityBottomReverseGroundTransfer
 import Zeta23.CCM.GlobalParityBottomTieNormalForm
 import Zeta23.CCM.GlobalParityBottomResidualState
+import Zeta23.CCM.GlobalParityBottomBranchPackage
+import Zeta23.CCM.GlobalParityBottomGroundTrial
 import Zeta23.CCM.GlobalParityBottomPrimeRemainder
 import Zeta23.CCM.GlobalParityBottomPrimeWeight
 import Zeta23.CCM.GlobalParityBottomArithmeticTarget
