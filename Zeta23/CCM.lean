96215bcfd2b5c2f1ea884f26e6f0eef031ec2d4b
Marcus Hermansson <marcus@hmwh.se>
2026-09-23 02:56:58 +0200

    PR #247: wire global-bottom residual state

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -170,6 +170,7 @@ import Zeta23.CCM.GlobalParityBottomEvenStrictNormalForm
 import Zeta23.CCM.GlobalParityBottomOddStrictNormalForm
 import Zeta23.CCM.GlobalParityBottomReverseGroundTransfer
 import Zeta23.CCM.GlobalParityBottomTieNormalForm
+import Zeta23.CCM.GlobalParityBottomResidualState
 import Zeta23.CCM.CrossParitySecularCompletion
 import Zeta23.CCM.CrossParitySecularKernelDichotomy
 import Zeta23.CCM.RegularFirstBadParitySourceMomentRigidity
