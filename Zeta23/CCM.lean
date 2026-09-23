e4cf46b7454c6ae7fd9615d403d2349c49b5622f
Marcus Hermansson <marcus@hmwh.se>
2026-09-23 02:19:15 +0200

    PR #247: wire strict-ground retained geometry

--- Zeta23/CCM.lean
+++ Zeta23/CCM.lean
@@ -165,6 +165,7 @@ import Zeta23.CCM.GlobalParityBottomIntertwining
 import Zeta23.CCM.GlobalParityBottomSourceMoment
 import Zeta23.CCM.GlobalFirstBadParityBottom
 import Zeta23.CCM.GlobalFirstBadParityBottomAlignment
+import Zeta23.CCM.GlobalParityBottomRetainedGeometry
 import Zeta23.CCM.CrossParitySecularCompletion
 import Zeta23.CCM.CrossParitySecularKernelDichotomy
 import Zeta23.CCM.RegularFirstBadParitySourceMomentRigidity
